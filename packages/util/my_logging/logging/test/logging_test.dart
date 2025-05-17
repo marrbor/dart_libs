3f5461f6f967600fe2c368c3fe633e95ee9889de
Masahide Matsumoto <matsumoto.masahide@gmail.com>
2025-05-18 00:44:07 +0900

    change lib name

--- packages/util/my_logging/logging/test/logging_test.dart
+++ packages/util/my_logging/logging/test/logging_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:logging/logging.dart';
+import 'package:my_logging/logging.dart';
 
 void main() {
   test('adds one to input values', () {
